56db8e5573f082d994c2bbd918e356d819b25399
tsukiyo <gd6l0o2v9e1@gmail.com>
2023-09-19 20:38:37 +0800

    add_chat

--- Take_Care/Program.cs
+++ Take_Care/Program.cs
@@ -1,7 +1,11 @@
+using Take_Care.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//加入 SignalR
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -22,5 +26,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//加入 Hub
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
