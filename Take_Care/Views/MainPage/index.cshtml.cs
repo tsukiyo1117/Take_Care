41d5241a07e8d81df5bac7f26d8243928d8bfa0d
rex8675 <rex8675@gmail.com>
2023-10-12 16:31:24 +0800

    10/12主頁

---
+++ Take_Care/Views/MainPage/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Take_Care.Views.主頁
+{
+    public class MainPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
