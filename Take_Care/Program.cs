22e68d4c783e61cefcca829727bd671af5b1ae4b
rex8675 <rex8675@gmail.com>
2023-10-13 17:07:51 +0800

    10/13 (更改登入的圖示和Program的路徑)

--- Take_Care/Program.cs
+++ Take_Care/Program.cs
@@ -38,7 +38,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=MainPage}/{action=Index}/{id?}");
 
 //¥[¤J Hub
 app.MapHub<ChatHub>("/chatHub");
