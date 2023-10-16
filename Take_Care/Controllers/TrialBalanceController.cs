2cfbdadda61c19c8b8964a0650ced589526dc720
秉翔 張 <ref.icebox@gmail.com>
2023-10-16 15:03:46 +0800

    layout Bug

--- Take_Care/Controllers/TrialBalanceController.cs
+++ Take_Care/Controllers/TrialBalanceController.cs
@@ -1,9 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Take_Care.Hubs;
+using Take_Care.Models;
 
 namespace Take_Care.Controllers {
     public class TrialBalanceController : Controller {
-        public IActionResult Index() {
-            return View();
+		//private readonly IHubContext<ChatHub> _hubContext;
+		private readonly TakeCareContext _context;
+
+		public TrialBalanceController(TakeCareContext context) {
+			_context = context;
+		}
+		public IActionResult Index() {
+            var query = from o in _context.Citizenshipstatuses
+                        select o;
+
+            //List<Citizenshipstatus> Citizenshipstatus = new List<Citizenshipstatus>() {
+                
+            //};
+
+
+
+            return View(query.ToList());
         }
     }
 }
