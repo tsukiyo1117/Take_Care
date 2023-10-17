ed6bac9b05c3766ef27840e2effedb80c2576140
rex8675 <rex8675@gmail.com>
2023-10-17 17:06:06 +0800

    10/17 新增四包錢相關東西，把主頁照片移到img裡的mainpage資料夾

---
+++ Take_Care/Controllers/FourpacksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Take_Care.Controllers
+{
+    public class FourpacksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
