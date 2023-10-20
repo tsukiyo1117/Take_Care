f7f4928abea8b9e338754979dc3b25944341c3c7
rex8675 <rex8675@gmail.com>
2023-10-20 14:34:43 +0800

    10/20 在主頁新增試算表的連結

---
+++ Take_Care/Controllers/APIController/FourpacksLongtermController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Take_Care.Controllers.APIController
+{
+    public class FourpacksLongtermController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Take_Care/Controllers/APIController/FourpacksOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Take_Care.Controllers.APIController
+{
+    public class FourpacksOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
