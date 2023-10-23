a3d2cdc66f1e3dc89cab03fe1a54eb8f3e8d49e6
DongHanWu <donghan110723@gmail.com>
2023-10-24 03:43:36 +0800

    弄了服務歷史紀錄頁面，表格有成功帶上資料庫的值，結帳按鈕也可以成功帶上內容值連到綠界，查詢和取消功能一直失敗

--- Take_Care/wwwroot/js/CsMember/DataWithAPI.js
+++ Take_Care/wwwroot/js/CsMember/DataWithAPI.js
@@ -71,6 +71,39 @@ const profile = Vue.createApp({
             this.fetchEmergencyContact();
             this.fetchEmployeeData();
         });
+        $("#twzipcode").twzipcode();
+        const userData = JSON.parse(sessionStorage.getItem('id'));
+        const county = userData[0].address.slice(0, 3);
+        const district = userData[0].address.slice(3);
+        // console.log(userData.address)
+        // console.log(userData.address.slice(0, 3))
+        // console.log(userData.address.slice(3))
+        // 使用 TWzipcode 的 .set() 方法設定地址
+        $("#twzipcode").twzipcode('set', {
+            'county': county,
+            'district': district
+        });
+
+
+        $("#twzipcode2").twzipcode();
+        const userData2 = JSON.parse(sessionStorage.getItem('personalResidentialAddress'));
+        const county2 = userData2.slice(0, 3);
+        const district2 = userData2.slice(3);
+        // console.log(county2)
+        $("#twzipcode2").twzipcode('set', {
+            'county': county2,
+            'district': district2
+        });
+
+
+        $("#twzipcode3").twzipcode();
+        const userData3 = JSON.parse(sessionStorage.getItem('personalMailingAddress'));
+        const county3 = userData3.slice(0, 3);
+        const district3 = userData3.slice(3)
+        $("#twzipcode3").twzipcode('set', {
+            'county': county3,
+            'district': district3
+        });
 
     },
     methods: {
@@ -354,7 +387,7 @@ const profile = Vue.createApp({
                 }
             });
         },
-
+        
     }
 });
 
@@ -428,6 +461,39 @@ const Csprofile = Vue.createApp({
             this.fetchIDData();
             this.fetchEmergencyContact();
             this.fetchEmployeeData();
+        });
+        $("#twzipcode").twzipcode();
+        const userData = JSON.parse(sessionStorage.getItem('id'));
+        const county = userData[0].address.slice(0, 3);
+        const district = userData[0].address.slice(3);
+        // console.log(userData.address)
+        // console.log(userData.address.slice(0, 3))
+        // console.log(userData.address.slice(3))
+        // 使用 TWzipcode 的 .set() 方法設定地址
+        $("#twzipcode").twzipcode('set', {
+            'county': county,
+            'district': district
+        });
+
+
+        $("#twzipcode2").twzipcode();
+        const userData2 = JSON.parse(sessionStorage.getItem('personalResidentialAddress'));
+        const county2 = userData2.slice(0, 3);
+        const district2 = userData2.slice(3);
+        // console.log(county2)
+        $("#twzipcode2").twzipcode('set', {
+            'county': county2,
+            'district': district2
+        });
+
+
+        $("#twzipcode3").twzipcode();
+        const userData3 = JSON.parse(sessionStorage.getItem('personalMailingAddress'));
+        const county3 = userData3.slice(0, 3);
+        const district3 = userData3.slice(3)
+        $("#twzipcode3").twzipcode('set', {
+            'county': county3,
+            'district': district3
         });
 
     },
@@ -717,4 +783,7 @@ const Csprofile = Vue.createApp({
 
 profile.mount("#profile");
 Csprofile.mount("#CsProfile");
+
+
+
 
